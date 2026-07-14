61a6fd5f23ca461ecd96ccecece1521b4e0f4cfb
Areesh Nadeem <areeshnm28@gmail.com>
2026-07-14 19:39:28 +0500

    Created Canvas

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,15 @@
+import Scene from "@/components/Scene"; 
+
 export default function Home() {
   return (
     <main className="w-screen h-screen">
+      
       <h1 className="text-4xl font-bold text-center mt-10">
         React Three Fiber Practice
       </h1>
+
+
+      <Scene />
     </main>
 
 
---
+++ src/components/Scene.tsx
@@ -0,0 +1,13 @@
+"use client";
+import {Canvas} from "@react-three/fiber";
+export default function Scene(){
+
+
+    return (
+
+        <Canvas>
+            
+        </Canvas>
+
+    )
+}
