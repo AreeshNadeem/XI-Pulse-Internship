82ce05d8a58b9611c1ad1ae211cf10543c4046de
Areesh Nadeem <areeshnm28@gmail.com>
2026-07-14 19:55:28 +0500

    Explored more functions

--- src/components/Scene.tsx
+++ src/components/Scene.tsx
@@ -1,11 +1,22 @@
 "use client";
 import {Canvas} from "@react-three/fiber";
+import {OrbitControls} from "@react-three/drei"; 
 export default function Scene(){
 
 
     return (
 
         <Canvas>
+            <gridHelper args={[10,10]} />
+            <axesHelper args={[5]} />
+            <OrbitControls />
+
+            <directionalLight position={[2,2,2]} />
+            <mesh position={[-2,0,0]} rotation={[0, Math.PI/2,0]} scale={2}>
+                <boxGeometry />
+                    <meshStandardMaterial color="orange" />
+                
+            </mesh>
             
         </Canvas>
 
