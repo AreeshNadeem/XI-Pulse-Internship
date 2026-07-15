7dc36604da77b8606b2d24c962bb0845e1d7500d
Areesh Nadeem <areeshnm28@gmail.com>
2026-07-15 20:46:59 +0500

    Used Environment, Spotlight, etc.

--- src/components/Scene.tsx
+++ src/components/Scene.tsx
@@ -2,7 +2,7 @@
 
 import { Canvas } from "@react-three/fiber";
 import { OrbitControls, Stage } from "@react-three/drei";
-
+import { Environment } from "@react-three/drei";
 import Factory from "./Factory";
 import House from "./House";
 
@@ -10,7 +10,8 @@ export default function Scene() {
   return (
 
   
-    
+    //different positions testing 
+
     //<Canvas camera={{ position: [5, 5, 5], fov: 50 }}>
     //<Canvas camera={{ position: [20, 15, 20], fov: 30 }  }> 
     //<Canvas camera={{ position: [0, -1, 0], fov: 30 }  }>  //this makes the camera got beneath the model
@@ -20,16 +21,28 @@ export default function Scene() {
 
     <Canvas camera={{ position: [10, 10, 10], fov: 50 }}>
       
-    // ambient light illumivates the entire scene  
+    {/* ambient light illumivates the entire scene */}
     <ambientLight intensity={1} />
 
-    //diretional light is specific which shines according to the position that is set
+    {/* directional light is specific which shines according to the position that is set */}
     <directionalLight 
     position={[10, 10, 10]} // (x, y, z)
-    intensity={2} />
+    intensity={2} /> 
 
+
+    {/* there are multiple presets and it sets the lighting, etc accordingly. it creates imaginary world around the model */}
+    <Environment preset="warehouse" />
+
+
+    {/* from the top by default and if pos changed can be from other positions too */}
+    {/* <spotLight intensity={10} position={[2, 2, 2]}/> */}
+
+
+    {/* draws line to see the 3 axes */}
     <axesHelper args={[5]} />
+    {/* draws a grid for better visualization */}
     <gridHelper args={[10, 10]} />
+
       
     <House/> 
     
